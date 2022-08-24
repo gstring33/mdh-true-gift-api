a4c6119bdcc6b366457f013a12bd93e2f3567788
Martin Dhenu <martin.dhenu@chefkoch.de>
2022-08-24 11:29:38 +0200

    Define unique constraint for email

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -41,7 +41,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private ?bool $isActive = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(length: 255, unique: true)]
     private ?string $email = null;
 
     #[ORM\OneToOne(targetEntity: self::class, cascade: ['persist', 'remove'])]
