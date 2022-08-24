4114b18cfe3840d6c1e91417bcf5ddd91341915f
Martin Dhenu <martin.dhenu@chefkoch.de>
2022-08-24 17:10:07 +0200

    Manage kernel exception

---
+++ src/EventListener/ExceptionListener.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\EventListener;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\Exception\HttpExceptionInterface;
+
+class ExceptionListener
+{
+    public function onKernelException(ExceptionEvent $event)
+    {
+        // You get the exception object from the received event
+        $exception = $event->getThrowable();
+
+        // Customize your response object to display the exception details
+        $response = new Response();
+        $response->setContent(json_encode(['error' => $exception->getMessage()]));
+
+        // HttpExceptionInterface is a special type of exception that
+        // holds status code and header details
+        if ($exception instanceof HttpExceptionInterface) {
+            $response->setStatusCode($exception->getStatusCode());
+            $response->headers->replace($exception->getHeaders());
+            $response->headers->add(['Content-Type'=> 'application/json']);
+        } else {
+            $response->headers->add(['Content-Type'=> 'application/json']);
+            $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
+
+        // sends the modified response object to the event
+        $event->setResponse($response);
+    }
+}
