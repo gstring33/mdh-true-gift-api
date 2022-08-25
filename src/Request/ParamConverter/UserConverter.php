8dcdd2e0add920be1e8ae8585ee32b436cb9c773
Martin Dhenu <martin.dhenu@chefkoch.de>
2022-08-25 12:03:29 +0200

    Get existing user in user converter

--- src/Request/ParamConverter/UserConverter.php
+++ src/Request/ParamConverter/UserConverter.php
@@ -4,17 +4,26 @@ namespace App\Request\ParamConverter;
 
 use App\Entity\GiftList;
 use App\Entity\User;
+use App\Repository\UserRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Request\ParamConverter\ParamConverterInterface;
 use Symfony\Component\HttpFoundation\Request;
 
 class UserConverter implements ParamConverterInterface
 {
+    private UserRepository $userRepository;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
     public function apply(Request $request, ParamConverter $configuration)
     {
         if ($this->isNewCreatedUser($request)) {
-            $data = json_decode($request->getContent(), true);
-            $user = $this->convertNewUser($data);
+            $user = $this->convertNewUser($request);
+        } else {
+            $user = $this->convertExistingUser($request);
         }
 
         $request->attributes->set($configuration->getName(), $user);
@@ -25,17 +34,22 @@ class UserConverter implements ParamConverterInterface
         return $configuration->getName() === 'user';
     }
 
-    private function isNewCreatedUser (Request $request)
+    /**
+     * @param Request $request
+     * @return bool
+     */
+    private function isNewCreatedUser (Request $request) : bool
     {
-        return $request->getMethod() === 'POST';
+        return $request->isMethod('POST');
     }
 
     /**
-     * @param array $data
+     * @param Request $request
      * @return User
      */
-    private function convertNewUser (array $data) : User
+    private function convertNewUser (Request $request) : User
     {
+        $data = json_decode($request->getContent(), true);
         $list = (new GiftList())
             ->setUuid(uniqid('', false))
             ->setIsPublished(0);
@@ -49,6 +63,18 @@ class UserConverter implements ParamConverterInterface
             ->setGiftList($list);
         $roles = !isset($roles['roles']) ? ['ROLE_USER'] : $roles['roles'];
         $user->setRoles($roles);
+
+        return $user;
+    }
+
+    /**
+     * @param Request $request
+     * @return User
+     */
+    private function convertExistingUser (Request $request) : User
+    {
+        $uuid = $request->attributes->get('uuid');
+        $user = $this->userRepository->findOneBy(['uuid' => $uuid]);
 
         return $user;
     }
