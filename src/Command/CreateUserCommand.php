80ca8c218849cb0b07c3055a80513684d862f54d
Martin Dhenu <martin.dhenu@chefkoch.de>
2022-08-25 15:30:36 +0200

    Create command for user creation

---
+++ src/Command/CreateUserCommand.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\GiftList;
+use App\Entity\User;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+#[AsCommand(
+    name: 'app:create-user',
+    description: 'Creates a new user.',
+    hidden: false,
+    aliases: ['app:add-user']
+)]
+class CreateUserCommand extends Command
+{
+    private UserPasswordHasherInterface $passwordHasher;
+    private ManagerRegistry $doctrine;
+
+    public function __construct(string $name = null, UserPasswordHasherInterface $passwordHasher, ManagerRegistry $doctrine)
+    {
+        parent::__construct($name);
+        $this->passwordHasher = $passwordHasher;
+        $this->doctrine = $doctrine;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $list = (new GiftList())
+            ->setUuid(uniqid('', false))
+            ->setIsPublished(0);
+        $user = (new User())
+            ->setFirstname('John')
+            ->setLastname('Doe')
+            ->setEmail('john.doe@t-online.de')
+            ->setRoles(['ROLE_ADMIN'])
+            ->setIsActive(0)
+            ->setGiftList($list)
+            ->setUuid(uniqid('', false));
+
+        $hashedPassword = $this->passwordHasher->hashPassword($user, '123456');
+        $user->setPassword($hashedPassword);
+
+        $em = $this->doctrine->getManager();
+        $em->persist($user);
+        $em->flush();
+
+        return Command::SUCCESS;
+    }
+}
