7567e1321ad4b4dae6d6a338e07fe85dc1d4183e
Martin Dhenu <martin.dhenu@chefkoch.de>
2022-09-15 12:23:09 +0200

    Create endpoint to create a gift

--- src/Controller/GiftController.php
+++ src/Controller/GiftController.php
@@ -2,13 +2,39 @@
 
 namespace App\Controller;
 
+use App\Entity\Gift;
+use App\Repository\UserRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
+
 
 #[Route('/api/gift', name: 'app_gift')]
 class GiftController extends AbstractController
 {
+    private ManagerRegistry $doctrine;
+    private JWTTokenManagerInterface $jwtManager;
+    private TokenStorageInterface $tokenStorage;
+    private UserRepository $userRepository;
+
+    public function __construct(
+        ManagerRegistry $doctrine,
+        JWTTokenManagerInterface $jwtManager,
+        TokenStorageInterface $tokenStorage,
+        UserRepository $userRepository
+
+    ) {
+        $this->doctrine = $doctrine;
+        $this->jwtManager = $jwtManager;
+        $this->tokenStorage = $tokenStorage;
+        $this->userRepository = $userRepository;
+    }
+
     #[Route('/{uuid}', name: 'app_gift_show', methods: ['GET'])]
     public function show(int $uuid): JsonResponse
     {
@@ -20,10 +46,28 @@ class GiftController extends AbstractController
         return $this->json($data);
     }
 
-    #[Route('/', name: 'app_gift_create', methods: ['POST'])]
-    public function create(): JsonResponse
+    #[Route('', name: 'app_gift_create', methods: ['POST'])]
+    #[ParamConverter('new-gift', class: 'App\Request\ParamConverter\NewGiftConverter')]
+    public function createGift(Request $request): JsonResponse
     {
-        return $this->json(['Gift created succesfully']);
+        /** @var Gift $gift */
+        $gift = $request->attributes->get('new-gift');
+        $decodedJwtToken = $this->jwtManager->decode($this->tokenStorage->getToken());
+        $uuid = $decodedJwtToken['uuid'];
+        $currentUser = $this->userRepository->findOneBy(['uuid' => $uuid]);
+
+        if (!$currentUser) {
+            return $this->json(['message'=> 'No User Found'], 404);
+        }
+
+        $list = $currentUser->getGiftList();
+        $gift->setGiftList($list);
+        $em = $this->doctrine->getManager();
+        $em->persist($gift);
+        $em->flush();
+
+        // TODO: fix circular reference with groups
+        return $this->json(['Success']);
     }
 
     #[Route('/{uuid}', name: 'app_gift_edit', methods: ['PUT'])]
