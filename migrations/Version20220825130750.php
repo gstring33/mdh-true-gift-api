95f0c44a256a5f039069f98556cbaebeaf13dae9
Martin Dhenu <martin.dhenu@chefkoch.de>
2022-10-13 10:54:11 +0200

    adjust charset and varchar size

--- migrations/Version20220825130750.php
+++ migrations/Version20220825130750.php
@@ -31,7 +31,7 @@ final class Version20220825130750 extends AbstractMigration
                 INDEX IDX_A47C990D51F42524 (gift_list_id), 
                 PRIMARY KEY(id)) 
             DEFAULT CHARACTER 
-            SET utf8mb4 
+            SET utf8 
             COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB'
         );
         $this->addSql('
@@ -41,7 +41,7 @@ final class Version20220825130750 extends AbstractMigration
                 uuid VARCHAR(255) NOT NULL, 
                 PRIMARY KEY(id)) 
             DEFAULT CHARACTER 
-            SET utf8mb4 
+            SET utf8 
             COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB'
         );
         $this->addSql('
@@ -52,12 +52,12 @@ final class Version20220825130750 extends AbstractMigration
                 gift_list_id INT DEFAULT NULL,
                 uuid VARCHAR(180) NOT NULL,
                 roles TEXT NOT NULL,
-                password VARCHAR(255) NOT NULL,
+                password VARCHAR(150) NOT NULL,
                 firstname VARCHAR(50) NOT NULL,
                 lastname VARCHAR(50) NOT NULL,
                 last_connection_at DATETIME DEFAULT NULL, 
                 is_active TINYINT(1) NOT NULL,
-                email VARCHAR(255) NOT NULL, 
+                email VARCHAR(100) NOT NULL, 
                 UNIQUE INDEX UNIQ_8D93D649D17F50A6 (uuid), 
                 UNIQUE INDEX UNIQ_8D93D649E7927C74 (email), 
                 UNIQUE INDEX UNIQ_8D93D6496EBA165A (recieve_gift_from_id), 
@@ -65,7 +65,7 @@ final class Version20220825130750 extends AbstractMigration
                 UNIQUE INDEX UNIQ_8D93D64951F42524 (gift_list_id), 
                 PRIMARY KEY(id))
             DEFAULT CHARACTER 
-            SET utf8mb4 
+            SET utf8 
             COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB'
         );
         $this->addSql('ALTER TABLE gift ADD CONSTRAINT FK_A47C990D51F42524 FOREIGN KEY (gift_list_id) REFERENCES gift_list (id)');
